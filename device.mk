48f42eb927c200a7a998e25c105e073e84bca0c9
chaptsand <chaptsand@gmail.com>
2021-08-14 14:31:40 +0800

    umi: Build fingerprintextension from source

--- device.mk
+++ device.mk
@@ -152,6 +152,7 @@ PRODUCT_PACKAGES += \
     vendor.aospa.biometrics.fingerprint.inscreen@1.0-service
 
 PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.xiaomi.hardware.touchfeature@1.0.vendor
 
