cbd6fe67614edecb6314856694cb344c01a73423
Robert <75765385+Robert-J19@users.noreply.github.com>
2021-09-08 23:46:11 +0800

    Update and rename radiant_umi.mk to wave_umi.mk

--- wave_umi.mk
+++ wave_umi.mk
@@ -11,15 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/umi/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/radiant/config/common_full_phone.mk)
+# Inherit some common WaveOS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/wave/configs/common.mk)
 
 # Include PixelExperience common configuration
-TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := radiant_umi
+PRODUCT_NAME := wave_umi
 PRODUCT_DEVICE := umi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10
