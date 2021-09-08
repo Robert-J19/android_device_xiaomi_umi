74a91bddc976b5900a9e658abd4263a7a85144d5
Robert <75765385+Robert-J19@users.noreply.github.com>
2021-09-09 00:11:25 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -220,7 +220,6 @@ PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_umi
     
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
     vendor/wave/configs/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # Fastbootd
