bd3b54dd97812187b454f1d4d2f032212495aa0c
Robert <75765385+Robert-J19@users.noreply.github.com>
2021-09-08 23:47:39 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/radiant_umi.mk
+    $(LOCAL_DIR)/wave_umi.mk
 
 COMMON_LUNCH_CHOICES := \
-    radiant_umi-user \
-    radiant_umi-userdebug \
-    radiant_umi-eng
+    wave_umi-user \
+    wave_umi-userdebug \
+    wave_umi-eng
